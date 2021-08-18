55f9d6e3b8b6422eddf2fef56318149087b3632e
Bruno Almeida da Silveira <dasilveira.bru@terra.com.br>
2021-08-18 00:41:01 -0300

    Add redirect script in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        })
+      }
+    })
+  }
   return (
     <div>
       Hello World!
