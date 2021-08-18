807f58b74a4f71d8f30e4aae16fbc154d66885bb
Bruno Almeida da Silveira <dasilveira.bru@terra.com.br>
2021-08-18 11:35:23 -0300

    Back to yaml

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,12 @@ import Image from 'next/image'
 import { useState } from 'react';
 import Markdown from 'react-markdown'
 import styles from '../styles/Home.module.css'
-//import postData from "../public/posts/2021-08-18-map-projectname-projeto-teste-2-texto-texto-de-teste-n-nto-aparecendo-na-tela-se-sim-deu-certoooo.json"
 
-/*
-<h2>{postData.projectname}</h2>
-      <Markdown children={postData.texto} />
-*/
+
+
 
 export default function Home() {
 
-  const [ post, setPost ] = useState({})
 
 
   return (
