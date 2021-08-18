e3a2854ef4e4dcb41623db0514b0c1de20092ffe
Bruno Almeida da Silveira <dasilveira.bru@terra.com.br>
2021-08-18 10:21:57 -0300

    Remove old post

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Image from 'next/image'
 import { useState } from 'react';
 import Markdown from 'react-markdown'
 import styles from '../styles/Home.module.css'
-import postData from "../public/posts/2021-08-18-map-projectname-projeto-teste-2-texto-texto-de-teste-n-nto-aparecendo-na-tela-se-sim-deu-certoooo.json"
+//import postData from "../public/posts/2021-08-18-map-projectname-projeto-teste-2-texto-texto-de-teste-n-nto-aparecendo-na-tela-se-sim-deu-certoooo.json"
 
+/*
+<h2>{postData.projectname}</h2>
+      <Markdown children={postData.texto} />
+*/
 
 export default function Home() {
 
@@ -14,8 +18,6 @@ export default function Home() {
   return (
     <div>
       <h1>Posts</h1>
-      <h2>{postData.projectname}</h2>
-      <Markdown children={postData.texto} />
       
     </div>
   )
