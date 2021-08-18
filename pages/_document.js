2789843596e94ae38bf7b7bfb2c989c50de7f93c
Bruno Almeida da Silveira <dasilveira.bru@terra.com.br>
2021-08-18 16:52:15 -0300

    changed to Script component

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head >
-            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
         </Head>
         <body>
           <Main />
