9ba056a6e26adda0ddfd00a9e9618118de513474
Bruno Almeida da Silveira <dasilveira.bru@terra.com.br>
2021-08-18 13:03:23 -0300

    posts.js to load json

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,27 @@ import Image from 'next/image'
 import { useState } from 'react';
 import Markdown from 'react-markdown'
 import styles from '../styles/Home.module.css'
+import { fetchPostContent } from "../lib/posts"
 
 
 
-
-export default function Home() {
-
+export default function Home({ posts }) {
 
 
+  console.log(posts)
   return (
     <div>
       <h1>Posts</h1>
-      
+  
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  const posts = fetchPostContent();
+  return {
+    props: {
+      posts
+    },
+  };
+};
